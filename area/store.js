624d7522cc9a4ef399b353dfaf1787416bbf520b
Shamim Ahmed <shamimahmed30082@gmail.com>
2022-12-03 10:10:07 +0600

    basic work done

---
+++ area/store.js
@@ -0,0 +1,315 @@
+var storeList =
+    [
+      {
+        "type": "Feature",
+        "properties": {"name": "story 1",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.3999397057791,
+            23.859364223550358
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "story 2",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.38638136821544,
+            23.87040535176888
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "story 3",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.36894921991961,
+            23.86355642051184
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "story 4",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.40575042187857,
+            23.86538677381438
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.38818914655815,
+            23.86574103275521
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.36804533074883,
+            23.857533785142564
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.35455155669683,
+            23.86361546457117
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.357327787722,
+            23.873003127481738
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.37113600578306,
+            23.776095642773953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.41847609238863,
+            23.734164143298116
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39862379800587,
+            23.71971794521565
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.35535597691455,
+            23.810096821855026
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.36604567389008,
+            23.832914539063808
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.37317213854021,
+            23.82313600866904
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.34415724675017,
+            23.780753864339886
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.34670241269725,
+            23.776095642773953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.35128371140019,
+            23.78727509417277
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.42814772298448,
+            23.849676018892893
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.40422316308735,
+            23.75699519017887
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.40880446179159,
+            23.776095642773953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39709669843734,
+            23.780753864339886
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39556959887005,
+            23.77982223337611
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39607863205873,
+            23.778424774415583
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39607863205873,
+            23.77795895142404
+          ],
+          "type": "Point"
+        }
+      }
+    ]
