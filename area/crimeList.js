2a5b2281fa8ecb85347c8919e38178eab1149180
Shamim Ahmed <shamimahmed30082@gmail.com>
2022-12-12 23:56:48 +0600

    final phase 1.0

---
+++ area/crimeList.js
@@ -0,0 +1,316 @@
+var crimeList =
+    [
+      {
+        "type": "Feature",
+        "properties": {"name": "Trucker responsible for injuring actress Ahona arrested",
+        "address": "Police yesterday arrested the trucker responsible for injuring television and film actress Ahona Rahman Lucky following a scuffle with her at Uttara in Dhaka on Wednesday.The driver, Sumon Mia, 27, was arrested in Ashulia around 4:30am, on information from his 16-year-old helper, who was held in Uttara on Friday night, Abdur Razzak, inspector (investigation) of Uttara West Police Station, told The Daily Star.“We have sent the helper to court where he gave confessional statement under section-164, accepting that they tried to drive the truck over Ahona",
+        "phone": "23 2323 2323", "comment": "this is a comment"},
+        
+        "geometry": {
+          "coordinates": [
+            90.3999397057791,
+            23.859364223550358
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Bomb disposal unit defuses 31 bombs found in Uttara: DB",
+        "address": "Lorem ipsum At least 31 crude bombs found from Uttara's sector-10 in the capital this evening have been defused by the bomb disposal unit of Counter Terrorism and Transnational Crime unit of Dhaka Metropolitan Police (DMP).Kazi Shafiqul Alam, deputy commissioner of Detective Branch (DB) of Police of Dhaka Metropolitan Police confirmed the development to The Daily Star.The bombs were recovered from holding-18 in road-13, Kazi Shafiqul Alam added.The bombs were recovered from information provided by two Jubo Dal men – Mamun, general secretary of Dhaka city's 54 no. ward and activist Sumon arrested earlier in the day, the official added. sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.38638136821544,
+            23.87040535176888
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "story 3",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.36894921991961,
+            23.86355642051184
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "story 4",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.40575042187857,
+            23.86538677381438
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.38818914655815,
+            23.86574103275521
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.36804533074883,
+            23.857533785142564
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.35455155669683,
+            23.86361546457117
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.357327787722,
+            23.873003127481738
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.37113600578306,
+            23.776095642773953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.41847609238863,
+            23.734164143298116
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39862379800587,
+            23.71971794521565
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.35535597691455,
+            23.810096821855026
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.36604567389008,
+            23.832914539063808
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.37317213854021,
+            23.82313600866904
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.34415724675017,
+            23.780753864339886
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.34670241269725,
+            23.776095642773953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.35128371140019,
+            23.78727509417277
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.42814772298448,
+            23.849676018892893
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.40422316308735,
+            23.75699519017887
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.40880446179159,
+            23.776095642773953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39709669843734,
+            23.780753864339886
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39556959887005,
+            23.77982223337611
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39607863205873,
+            23.778424774415583
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {"name": "Story ....",
+        "address": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut animi voluptate facere, fugiat alias perspiciatis aperiam doloribus, iusto impedit, ipsam sapiente voluptatum quisquam eum repellat. Vero ipsam iste ratione placeat?",
+        "phone": "23 2323 2323"},
+        "geometry": {
+          "coordinates": [
+            90.39607863205873,
+            23.77795895142404
+          ],
+          "type": "Point"
+        }
+      }
+    ]
---
+++ index.js
@@ -0,0 +1,192 @@
+var map = L.map('map').setView([23.788212, 90.399971], 14);
+
+    const key = 'kyStKR2HkmDzAQngf5FW';
+    var bright = L.tileLayer(`https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}.png?key=kyStKR2HkmDzAQngf5FW`,{ //style URL
+        tileSize: 512,
+        zoomOffset: -1,
+        minZoom: 1,
+        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+        crossOrigin: true
+    });
+
+    /*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+    */
+
+    var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxNativeZoom: 19, 
+    maxZoom: 20 }).addTo(map);
+    
+    var CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+	maxZoom: 20});
+
+    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+
+    
+    navigator.geolocation.watchPosition(success, error);
+
+    var marker, circle;
+
+
+   
+
+    function success(pos){
+        let lat, lng;
+        lat = pos.coords.latitude;
+        console.log(lat);
+        
+        lng = pos.coords.longitude;
+        console.log(lng);
+        const accuracy = pos.coords.accuracy;
+
+        if(marker){
+            map.removeLayer(marker);
+            map.removeLayer(circle);
+        }
+        marker = L.marker([lat, lng]).addTo(map);
+        //circle = L.circle([lat, lng], {radius : accuracy}).addTo(map);
+
+        //map.fitBounds(circle.getBounds())
+    }
+
+    function error(err){
+        if(err.code === 1){
+            alert("please allow location access");
+        }
+        else{
+            alert("can't get final location");
+        }
+
+
+    }
+
+    
+
+    
+    function style(feature) {
+        return {
+            fillColor: 'green',
+            weight: 2,
+            opacity: 100,
+            color: 'black',
+            dashArray: '3',
+            fillOpacity: 0
+        };
+    }
+    function style2(feature) {
+        return {
+            fillColor: 'red',
+            weight: 1,
+            opacity: 100,
+            color: 'black',
+            dashArray: '3',
+            fillOpacity: 0.9
+        };
+    }
+    
+    
+    L.geoJSON(dhakaCityCorporation, {style:style}).addTo(map);
+            
+        
+    // function myFunction() {
+        
+    //     L.geoJSON(crime, myLayerOptions, markers).addTo(map);
+                
+    // }
+
+
+    var markers = L.markerClusterGroup();
+            
+    function createCustomIcon (feature, latlng) {
+        let myIcon = L.icon({
+        iconUrl: 'images/crime.png',
+        shadowUrl: 'images/leaf-shadow.png',
+        iconSize:     [35, 40], // width and height of the image in pixels
+        shadowSize:   [35, 20], // width, height of optional shadow image
+        iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+        shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
+        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+        })
+        return markers.addLayer(L.marker(latlng, { icon: myIcon }))
+    }
+
+    
+    let myLayerOptions = {
+        pointToLayer: createCustomIcon
+    }
+
+    L.geoJSON(crime, myLayerOptions, markers).addTo(map);
+
+    // create the GeoJSON layer
+    //var crimes = L.geoJSON(crime, myLayerOptions, markers);      
+    //map.addLayer(markers);
+
+    let crimes;
+    function showArea() {
+        //L.geoJSON(uttaraSector3, {style:style}).addTo(map);
+        L.geoJSON(dhanmondi,{style:style2}).addTo(map);
+        L.geoJSON(bashundhara, {style:style2}).addTo(map);
+        L.geoJSON(kalabagan, {style:style2}).addTo(map);
+        L.geoJSON(purbachal, {style:style2}).addTo(map);
+        L.geoJSON(lalmatia, {style:style2}).addTo(map);
+        L.geoJSON(shyamoli, {style:style2}).addTo(map);
+        L.geoJSON(jigatola, {style:style2}).addTo(map);
+        L.geoJSON(sherEbanglaNagar, {style:style2}).addTo(map);
+        L.geoJSON(monipuripara, {style:style2}).addTo(map);
+        L.geoJSON(motijheel, {style:style2}).addTo(map);
+        L.geoJSON(uttara, {style:style2}).addTo(map);
+        L.geoJSON(ramna, {style:style2}).addTo(map);
+    }
+
+    var baseMaps = {
+    "OpenStreetMap": osm,
+    "CycleOsm":CyclOSM,
+    "satelite": Esri_WorldImagery,
+    "Bright":bright
+    //"crime": crimes
+    
+    };
+
+    
+
+    var layerControl = L.control.layers(baseMaps).addTo(map);
+
+
+
+
+
+    function generateList() {
+        const ul = document.querySelector('.list');
+        crimeList.forEach((crime) => {
+            const li = document.createElement('li');
+            const div = document.createElement('div');
+            const a = document.createElement('a');
+            const p = document.createElement('p');
+            a.addEventListener('click', () => {
+                flyToStore(crime);
+            });
+            div.classList.add('crime');
+            a.innerText = crime.properties.name;
+            a.href = '#';
+            p.innerText = crime.properties.address;
+
+            div.appendChild(a);
+            div.appendChild(p);
+            li.appendChild(div);
+            ul.appendChild(li);
+        });
+    }
+
+    generateList();
+
+
+    function flyToStore(c) {
+        const lat = c.geometry.coordinates[1];
+        const lng = c.geometry.coordinates[0];
+        map.flyTo([lat, lng], 18, {
+            duration: 2
+        });
+    }
